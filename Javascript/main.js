f37bf36849233cbf7481a2049c9d1657960d83f8
Lee Lawler <lawler_lee@icloud.com>
2020-02-03 17:12:01 -0600

    added javascript/firebase/css/html

---
+++ Javascript/main.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCMCtFDFzlzJh6qrWFZ_pqFJ-ELhweBNkY",
+    authDomain: "project-1-608e1.firebaseapp.com",
+    databaseURL: "https://project-1-608e1.firebaseio.com",
+    projectId: "project-1-608e1",
+    storageBucket: "project-1-608e1.appspot.com",
+    messagingSenderId: "510274083998",
+    appId: "1:510274083998:web:422fb226e2251b2f22ef8f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
